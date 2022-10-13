088b5251c73b829f28e2cec3b1d17b961c032471
Vvaridus <40428574@live.napier.ac.uk>
2022-10-13 13:19:55 +0100

    minor changes
    minor updates to TimeTablesCalculator.cs
    correct / incorrect answer based on user input and feedback given

--- Assets/Scripts/TimeTablesCalculator.cs
+++ Assets/Scripts/TimeTablesCalculator.cs
@@ -6,7 +6,9 @@ using TMPro;
 public class TimeTablesCalculator : MonoBehaviour
 {
     public TextMeshProUGUI firstNumber;
-    public TextMeshProUGUI secondNumber;    
+    public TextMeshProUGUI secondNumber;
+
+    public TextMeshProUGUI answerOutput;
 
     public TMP_InputField userAnswer;
 
@@ -30,14 +32,25 @@ public class TimeTablesCalculator : MonoBehaviour
         firstNumber.text = firstIntNumber.ToString();
         secondNumber.text = secondIntNumber.ToString();
 
+        //testing call needs changed to be active on user input
         Debug.Log("Answer = " + answerInt);
-
-        SubmitAnswer();
+        //SubmitAnswer();
     }
 
     public void SubmitAnswer()
     {
         int.TryParse(userAnswer.text, out userIntAnswer);
         Debug.Log("User Input: " + userIntAnswer);
+
+        if (answerInt == userIntAnswer)
+        {
+            //correct
+            answerOutput.text = "Correct! " + firstIntNumber.ToString() + " x " + secondIntNumber.ToString() + " = " + answerInt.ToString();
+        }
+        else
+        {
+            //incorrect
+            answerOutput.text = "Incorrect " + firstIntNumber.ToString() + " x " + secondIntNumber.ToString() + " = " + answerInt.ToString();
+        }
     }
 }
